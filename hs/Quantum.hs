0de6adfb8be6b9039bc92bd6eda18834d9ffa1c7
lfkremer <lfkremer@lfk.lfk>
2023-06-30 00:08:01 -0300

    Jun29

--- hs/Quantum.hs
+++ hs/Quantum.hs
@@ -7,13 +7,14 @@ import Data.IORef
 import System.Random
 
 
--- 2.1
 data Move = Vertical | Horizontal deriving (Show,Eq,Ord)
 data Rotation = CtrClockwise | Clockwise deriving (Show,Eq)
 data Color = Red | Yellow | Blue deriving (Show,Eq)
 
 data Qop a b = Qop (Map (a,b) PA)
 data QR a = QR (IORef (QV a))
+data Adaptor l g = Adaptor { dec :: g -> l, cmp :: l -> g }
+data Virt a na u = Virt (QR u) (Adaptor (a,na) u)
 
 -- Basis will also be an instance of Eq and Ord
 class (Eq a, Ord a) => Basis a where
@@ -143,8 +144,6 @@ observeV ::  Basis a => QV a -> IO a
 observeV v = do
   let nv = normalize v
       probs = Data.List.map (((\x -> x * x) . magnitude) . pr nv) basis
-      --probs = [(((\x -> x * x) . magnitude) . pr nv) basis | basis <- keys v]
-	  -- wrong comprehension
   r <- getStdRandom (randomR (0.0,1.0))
   let cPsCs = zip (scanl1 (+) probs) basis
       Just (_,res) = find (\(p,_) -> r < p) cPsCs
@@ -158,18 +157,97 @@ test = do
   print (o1,o2,o3) 
 
 
---{-
--- NOT WORKING
 observeLeft :: (Basis a, Basis b) => QR (a,b) -> IO a
 observeLeft (QR ptr) = do
   v <- readIORef ptr
-  let leftF a = sqrt (sum [((\x -> x * x) . magnitude) (pr v (a,b)) | b <- basis])
+  let leftF a = sqrt (sum [((\x -> x * x) . magnitude) (pr v (a,b)) | b <- basis]) :+ 0
       leftV = qv [(a, leftF a) | a <- basis]
   aobs <- observeV leftV
   let nv = qv [((aobs,b), pr v (aobs,b)) | b <- basis]
   writeIORef ptr (normalize nv)
   return aobs 
--- -}
 
 -- 5
+v_op :: Qop Bool Bool
+v_op = qop [((False,False),1),
+            ((True,True),0:+ 1)]
 
+vt_op :: Qop Bool Bool
+vt_op = qop [((False,False),1),
+             ((True,True),0:+ -1)]
+
+--5.1
+mkVirt1 :: QR(((a,b,c),(d,e)),(f,g)) -> Virt (d,g) (a,b,c,e,f) (((a,b,c),(d,e)),(f,g))
+mkVirt1 r = Virt r a_1
+  where a_1 = Adaptor { dec = \ (((a,b,c),(d,e)),(f,g)) -> ((d,g),(a,b,c,e,f)),
+                        cmp = \ ((d,g),(a,b,c,e,f)) -> (((a,b,c),(d,e)),(f,g)) }
+
+--mkVirt2 :: QR(((a,b,c),(d,e)),(f,g)) -> Virt (d,g) (a,b,c,e,f) (((a,b,c),(d,e)),(f,g))
+
+-- 5.2
+ad_triple_3 :: Adaptor (a3,(a1,a2)) (a1,a2,a3)
+ad_triple_3 = Adaptor { dec = \(a1,a2,a3) -> (a3,(a1,a2)),
+                       cmp = \(a3,(a1,a2)) -> (a1,a2,a3)}
+
+ad_triple_23 :: Adaptor ((a2,a3),a1) (a1,a2,a3)
+ad_triple_23 = Adaptor { dec = \(a1,a2,a3) -> ((a2,a3),a1),
+                         cmp = \((a2,a3),a1) -> (a1,a2,a3)}
+
+ad_triple_12 :: Adaptor ((a1,a2),a3) (a1,a2,a3)
+ad_triple_12 = Adaptor { dec = \(a1,a2,a3) -> ((a1,a2),a3),
+                         cmp = \((a1,a2),a3) -> (a1,a2,a3)}
+ad_triple_13 :: Adaptor ((a1,a3),a2) (a1,a2,a3)
+ad_triple_13 = Adaptor { dec = \(a1,a2,a3) -> ((a1,a3),a2),
+                         cmp = \((a1,a3),a2) -> (a1,a2,a3)}
+-- 5.3
+
+virtFromR :: QR a -> Virt a () a
+virtFromR r = Virt r (Adaptor {dec = \a -> (a,()),
+                               cmp = \(a,()) -> a})
+
+virtFromV :: Virt a na u -> Adaptor (a1,a2) a -> Virt a1 (a2,na) u
+virtFromV (Virt r (Adaptor { dec = gdec, cmp = gcmp}))
+          (Adaptor {dec = ldec, cmp = lcmp}) = 
+          Virt r (Adaptor {dec = \u -> let (a,na) = gdec u
+                                           (a1,a2) = ldec a 
+                                       in (a1,(a2,na)),
+                           cmp = \(a1,(a2,na)) -> gcmp (lcmp (a1,a2), na)})
+
+app :: (Basis a, Basis b, Basis nab,
+        Basis ua, Basis ub) => 
+  Qop a b -> Virt a nab ua -> Virt b nab ub -> IO ()
+app (Qop f)
+    (Virt (QR ra)
+          (Adaptor { dec = deca, cmp = cmpa}))
+    (Virt (QR rb)
+          (Adaptor { dec = decb, cmp = cmpb})) = 
+  let gf = qop [((ua,ub), pr f (a,b)) | 
+                ua <- basis, ub <- basis,
+                let (a,na) = deca ua,
+                let (b,nb) = decb ub,
+                na == nb]
+  in do fa <- readIORef ra
+        let fb = normalize $ qApp gf fa
+        writeIORef rb fb
+
+app1 :: (Basis, a, Basis na, Basis ua) => Qop a a -> Virt a na ua -> IO ()
+app1 f v = app f v v
+
+toffoli1 :: (Basis na, Basis u) => Virt (Bool,Bool,Bool) na u -> IO ()
+
+toffoli1 vtriple = 
+  let b = virtFromV vtriple ad_triple_3
+      mb = virtFromV vtriple ad_triple_23
+      tm = virtFromV vtriple ad_triple_12
+      tb = virtFromV vtriple ad_triple_13
+      cv = cop id v_op
+      cvt = cop id vt_op
+  in do app1 hadamard_op b
+        app1 cv mb
+		app1 cnot tm
+		app1 cvt mb
+		app1 cnot tm
+		app1 cv tb
+		app1 hadamard_op b 
+
+-- TODO : Apply toffoli to a 3-qbit
